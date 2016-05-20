2e867a614f4cfb96f1bee897e3736ca1dbdba1df
LWZ <lwzustc@gmail.com>
2016-05-20 22:48:42 +0800

    solved performance problem

--- js/page_transition.js
+++ js/page_transition.js
@@ -1,11 +1,21 @@
-$(function() {
-	$('a #top_button, a #back').click(function() {
-		var dest = $(this).parent().attr('href');
-		$.mobile.changePage( dest, {
-			transition: "slidedown",
-		});
-		setTimeout(function(){
-			window.location.href = dest;
-		}, 1000);
+// $(function() {
+// 	$('a #top_button, a #back').click(function() {
+// 		var dest = $(this).parent().attr('href');
+// 		$.mobile.changePage( dest, {
+// 			transition: "slidedown",
+// 		});
+// 		setTimeout(function(){
+// 			window.location.href = dest;
+// 		}, 1000);
+// 	});
+// });
+
+$('a #top_button, a #back').click(function() {
+	var dest = $(this).parent().attr('href');
+	$.mobile.changePage( dest, {
+		transition: "slidedown",
 	});
+	setTimeout(function(){
+		window.location.href = dest;
+	}, 1000);
 });
