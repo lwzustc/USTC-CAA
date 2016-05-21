97492a737dd12d87a0d298a38edb87cf27c9c9fd
LWZ <lwzustc@gmail.com>
2016-05-21 15:26:44 +0800

    fixed transitions; no freez, splash left

--- js/page_transition.js
+++ js/page_transition.js
@@ -1,9 +1,25 @@
-$('a #top_button, a #back').click(function() {
-	var dest = $(this).parent().attr('href');
+$('a div').click(function() {
+	var dest = $(this).attr('href');
 	$.mobile.changePage( dest, {
-		transition: "slidedown",
+		transition: transition_by_id($(this).attr('id')),
+		// role: 'page';
+		// reloadPage: true
 	});
-	setTimeout(function(){
-		window.location.href = dest;
-	}, 1000);
+	// setTimeout(function() {
+	// 	window.location.href = dest;
+	// }, 1000);
 });
+
+function transition_by_id(id) {
+	return 'slidedown';
+	if (id === 'back' || id === 'top_button') {
+		return 'slidedown';
+	}
+	else if (id === 'secondhand_exchangecode') {
+		return 'pop';
+	}
+	else {
+		return $.mobile.defaultPageTransition;
+	}
+	// 'fade'
+}
